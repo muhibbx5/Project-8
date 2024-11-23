a933c6473eca0ee337251bed315a3f851a2c800b
muhibbx5 <muhibbali56@gmail.com>
2024-11-23 11:47:08 +0500

    commit

--- main.py
+++
@@ -1,8 +0,0 @@
-# Ask the user to input a number
-n = input("Enter a word: ")
-
-# Count the number of digits
-digit_count = len(n)
-
-# Print the total number of digits
-print("Total digits:", digit_count)
